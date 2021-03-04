ec869dff689d041ca81405c7b469bc3f013db0aa
Francisco Ulloa <fulloa@lafuenteindustries.com>
2021-03-04 12:18:38 -0500

    [ADD] owl mixin/component to front

---
+++ my_theme/static/src/js/mixin.js
@@ -0,0 +1,32 @@
+odoo.define('my_theme.mixin_test', function (require) {
+    "use strict";
+
+    // my_addons/my_theme/static/src/js/mixin.js
+
+    const { ComponentAdapter, ComponentWrapper, WidgetAdapterMixin } = require('web.OwlCompatibility');
+    const { Component, tags, useState } = owl;
+    const { xml } = tags;
+
+    const publicWidget = require('web.public.widget');
+
+    const WidgetAdapter = publicWidget.Widget.extend(WidgetAdapterMixin, {
+        selector: '#mixinComponent',
+        disabledInEditableMode: false,
+
+        destroy() {
+            this._super(...arguments);
+            WidgetAdapterMixin.destroy.call(this, ...arguments);
+        },
+    });
+
+    class MyComponent extends Component {}
+    MyComponent.template = xml`<h1>inyectado!</h1>`;
+    const MyWidget = WidgetAdapter.extend({
+        start() {
+            const component = new ComponentWrapper(this, MyComponent, {});
+            return component.mount(this.el);
+        }
+    });
+
+    publicWidget.registry.mixin_test = MyWidget;
+});
